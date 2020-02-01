ca75f27f4b836b3caef65caae869600b6e39f274
Harika <17211a05h0@bvrit.ac.in>
2020-02-01 14:47:10 +0530

    Added java programs
    the files are Chef.java and Chef.class

---
+++ Chef.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+class Chef{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			float count=0;
+			int m=sc.nextInt();
+			float n=sc.nextFloat();
+			float k=sc.nextFloat();
+			float l=sc.nextFloat();
+			while(n>0){
+				if(n-l >=0){
+					count=l+count;
+					n=n-l;
+
+				}
+				else{
+					count+=n;
+				}
+				if(n-k>=0){
+					n=n-k;
+				}
+			}
+			int p=(int)count;
+			System.out.println(p);
+		}
+	}
+}
+					
